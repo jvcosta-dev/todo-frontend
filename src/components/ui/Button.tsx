8c7143ba7f6d7b727963cb262e388f3841155a39
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-10-22 15:55:22 -0300

    click prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,17 +4,19 @@ interface Props {
   type: "submit" | "button";
   full?: boolean;
   disabled?: boolean;
+  click?: () => void;
   children: ReactNode;
 }
 
-export function Button({ type, full, disabled, children }: Props) {
+export function Button({ type, full, disabled, click, children }: Props) {
   return (
     <button
       type={type}
       disabled={disabled}
+      onClick={click}
       className={`${
         full ? "w-full" : "w-max"
-      } flex justify-center px-4 py-1 rounded-lg bg-primary text-white capitalize disabled:bg-light disabled:text-dark transition-colors duration-150`}
+      } flex h-max justify-center px-4 py-1 rounded-lg bg-primary text-white capitalize disabled:bg-light disabled:text-dark transition-colors duration-150`}
     >
       {children}
     </button>
