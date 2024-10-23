fb443ddfe331509c405bd52b986010292bac0469
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-10-23 15:39:30 -0300

    formatted date in browser loc

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,20 +1,24 @@
-export const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
+export const formatDate = (
+  dateString: string,
+  locale: string = navigator.language
+) => {
+  const date = new Date(dateString); // A data está em UTC
 
   if (isNaN(date.getTime())) {
     return "Invalid date";
   }
 
-  const month = date.toLocaleString("en-US", { month: "short" });
+  const month = date.toLocaleString(locale, { month: "short" });
   const day = date.getDate();
 
   const options: Intl.DateTimeFormatOptions = {
     hour: "2-digit",
     minute: "2-digit",
     hour12: true,
+    timeZoneName: "short",
   };
-  const time = date.toLocaleString("en-US", options);
 
-  const [hours, minutes] = time.split(":");
-  return `${month} ${day} ${hours}:${minutes}`;
+  const time = date.toLocaleString(locale, options);
+
+  return `${month} ${day} ${time}`;
 };
