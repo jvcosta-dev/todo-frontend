721ba8397a181f0673b0fb30baa9a744808fd42d
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-10-24 15:51:47 -0300

    feat: page component

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -1,19 +1,22 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useContent } from "../contexts/ContentContext";
 import { Button } from "../components/ui/Button";
+import Page from "../components/Page";
 
 export function Notfound() {
   const { fallback } = useContent();
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-4">
-      <h1 className="text-8xl md:text-9xl">404</h1>
-      <p className="text-lg sm:text-2xl capitalize">{fallback.message}</p>
+    <Page title="Not Found" description={fallback.message}>
+      <div className="h-screen flex flex-col justify-center items-center gap-4">
+        <h1 className="text-8xl md:text-9xl">404</h1>
+        <p className="text-lg sm:text-2xl capitalize">{fallback.message}</p>
 
-      <Button click={() => navigate(-1)} type="button">
-        Go Back
-      </Button>
-    </div>
+        <Button click={() => navigate(-1)} type="button">
+          Go Back
+        </Button>
+      </div>
+    </Page>
   );
 }
