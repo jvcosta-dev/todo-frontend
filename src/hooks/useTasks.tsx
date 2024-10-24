913c984ead8430a435e3c9f954274ceac9adf947
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-10-24 18:15:52 -0300

    task refactor

--- src/hooks/useTasks.tsx
+++ src/hooks/useTasks.tsx
@@ -1,4 +1,3 @@
-// hooks/useTasks.ts
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAuth } from "../contexts/AuthContext";
 import { ITask, ITaskInput } from "../interfaces";
@@ -7,16 +6,33 @@ export function useTasks() {
   const queryClient = useQueryClient();
   const { fetchWithAuth } = useAuth();
 
-  const getTasks = () => fetchWithAuth<ITask[]>("mytasks").then((res) => res);
-
-  const { data, isLoading } = useQuery({
-    queryKey: ["tasks"],
-    queryFn: getTasks,
-  });
-
   const createTask = (data: ITaskInput) =>
     fetchWithAuth<null>("task", {
       method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "Application/JSON",
+      },
+    }).then((res) => res);
+
+  const getTasks = () => fetchWithAuth<ITask[]>("mytasks").then((res) => res);
+
+  const getTaskById = (taskId: string) =>
+    fetchWithAuth<ITask>(`task/${taskId}`).then((res) => res);
+
+  const deleteTask = (taskId: string) =>
+    fetchWithAuth<null>(`task/${taskId}`, { method: "DELETE" }).then(
+      (res) => res
+    );
+
+  const checkTask = (taskId: string) =>
+    fetchWithAuth<null>(`check/${taskId}`, { method: "PATCH" }).then(
+      (res) => res
+    );
+
+  const editTask = ({ taskId, data }: { taskId: string; data: ITaskInput }) =>
+    fetchWithAuth<null>(`task/${taskId}`, {
+      method: "PATCH",
       body: JSON.stringify(data),
       headers: {
         "Content-Type": "Application/JSON",
@@ -28,9 +44,41 @@ export function useTasks() {
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
   });
 
+  const checkMutation = useMutation({
+    mutationFn: checkTask,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteTask,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
+  });
+
+  const editMutation = useMutation({
+    mutationFn: editTask,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["tasks"] }),
+  });
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: getTasks,
+  });
+
+  const useTaskById = (taskId: string) => {
+    return useQuery({
+      queryKey: ["task", taskId],
+      queryFn: () => getTaskById(taskId),
+      enabled: !!taskId,
+    });
+  };
+
   return {
     tasks: data,
     isLoading,
     createTask: createMutation.mutate,
+    deleteTask: deleteMutation.mutate,
+    checkTask: checkMutation.mutate,
+    editTask: editMutation.mutate,
+    useTaskById,
   };
 }
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,21 +1,47 @@
 import { TaskList } from "../components/tasks/TaskList";
 import { Button } from "../components/ui/Button";
-import { FilePlusIcon } from "lucide-react";
-import { useState } from "react";
+import { FilePlusIcon, Search, Trash2 } from "lucide-react";
+import { FormEvent, useState } from "react";
 import { useTasks } from "../hooks/useTasks";
 import { CreateTaskModal } from "../components/tasks/CreateTask";
 import { Loading } from "../components/ui/Loading";
+import Page from "../components/Page";
+import { useAuth } from "../contexts/AuthContext";
+import { Input } from "../components/ui/Input";
+import { ITask } from "../interfaces";
+import { Task } from "../components/tasks/Task";
 
 export function Tasks() {
+  const { user } = useAuth();
   const { tasks, isLoading, createTask } = useTasks();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [term, setTerm] = useState("");
+  const [resultTask, setResultTask] = useState<ITask | null>(null);
+  const [touchedSearch, setTouchedSearch] = useState(false);
+
+  const search = (e: FormEvent) => {
+    e.preventDefault();
+    if (!tasks || !term) return;
+    const found = tasks.find((t) => t.title.includes(term));
+    setTouchedSearch(true);
+    if (found) setResultTask(found);
+    return;
+  };
 
   if (isLoading) return <Loading />;
 
   return (
-    <>
-      <div className="flex flex-col flex-grow">
-        <div className="flex justify-end">
+    <Page title="Your Tasks" description="manage all of your tasks.">
+      <div className="flex flex-col flex-grow gap-4">
+        <div className="flex flex-col md:flex-row items-center gap-4 md:justify-between">
+          <h1 className="text-xl">
+            Hello {user.name},{" "}
+            {tasks && tasks.length > 0
+              ? `you have ${tasks.length} tasks!`
+              : "you dont have any tasks!"}
+          </h1>
           <Button type="button" click={() => setIsModalOpen(true)}>
             <div className="flex gap-2 items-center">
               Create Task
@@ -25,7 +51,44 @@ export function Tasks() {
         </div>
 
         {tasks && tasks.length > 0 && (
-          <section className="flex flex-col flex-grow gap-6">
+          <>
+            <form
+              onSubmit={search}
+              className="flex items-center gap-2 rounded-xl"
+            >
+              <Input
+                name="term"
+                type="text"
+                value={term}
+                placeholder="search task by name"
+                change={(e) => setTerm(e.target.value)}
+              />
+              <Button type="submit">
+                <Search aria-label="search task" />
+              </Button>
+              <Button
+                type="button"
+                click={() => {
+                  setResultTask(null);
+                  setTouchedSearch(false);
+                  setTerm("");
+                }}
+                danger
+                disabled={!resultTask && !term && !touchedSearch}
+              >
+                <Trash2 aria-label="delete search results" />
+              </Button>
+            </form>
+            {resultTask ? (
+              <Task task={resultTask} />
+            ) : (
+              touchedSearch && (
+                <p className="text-danger text-center">
+                  No results for: {term}
+                </p>
+              )
+            )}
+
             <TaskList
               title="Pending Tasks"
               tasks={tasks.filter((t) => t.status === 2)}
@@ -38,7 +101,14 @@ export function Tasks() {
               title="Completed Tasks"
               tasks={tasks.filter((t) => t.status === 1)}
             />
-          </section>
+            {Array.from(new Set(tasks.map((t) => t.tag))).map((tag) => (
+              <TaskList
+                key={tag}
+                title={`#${tag} Tasks`}
+                tasks={tasks.filter((t) => t.tag === tag)}
+              />
+            ))}
+          </>
         )}
       </div>
 
@@ -47,6 +117,6 @@ export function Tasks() {
         onClose={() => setIsModalOpen(false)}
         onSubmit={createTask}
       />
-    </>
+    </Page>
   );
 }
