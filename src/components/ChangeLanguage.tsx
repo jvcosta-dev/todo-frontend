eb0fcb5595dcb1962d1e48356724ccaa36941590
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2024-11-07 15:10:51 -0300

    language label and css fix

--- src/components/ChangeLanguage.tsx
+++ src/components/ChangeLanguage.tsx
@@ -1,22 +1,26 @@
 import { useTranslation } from "react-i18next";
 
 export function ChangeLanguage() {
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   const changeLanguage = (lng: any) => {
     i18n.changeLanguage(lng);
     localStorage.setItem("language", lng);
   };
   return (
-    <select
-      className="p-1 border-2 rounded-lg bg-solid dark:bg-solidDark border-light dark:border-solidDark outline-none"
-      onChange={(e) => changeLanguage(e.target.value)}
-      defaultValue={i18n.language}
-    >
-      <option value="pt">Português</option>
-      <option value="en">English</option>
-      <option value="fr">Français</option>
-      <option value="es">Español</option>
-    </select>
+    <label htmlFor="changeLanguage" className="flex flex-col gap-1">
+      {t("language")}
+      <select
+        id="changeLanguage"
+        className="p-1 border-2 rounded-lg bg-solid dark:bg-solidDark border-light dark:border-solidDark outline-none"
+        onChange={(e) => changeLanguage(e.target.value)}
+        defaultValue={i18n.language}
+      >
+        <option value="pt">Português</option>
+        <option value="en">English</option>
+        <option value="fr">Français</option>
+        <option value="es">Español</option>
+      </select>
+    </label>
   );
 }
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -19,8 +19,11 @@ export function Settings() {
 
   return (
     <Page title={t("title-settings")} description={t("desc-settings")}>
-      <div className="inline-flex items-center gap-2">
-        <label className="flex items-center cursor-pointer relative">
+      <label
+        htmlFor="check"
+        className="flex items-center gap-1 cursor-pointer relative"
+      >
+        <div className="inline-flex items-center">
           <input
             onChange={switchDarkMode}
             checked={darkMode}
@@ -29,25 +32,9 @@ export function Settings() {
             className="peer h-6 w-6 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-primary checked:border-primary"
             id="check"
           />
-          <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-3.5 w-3.5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              stroke="currentColor"
-              strokeWidth="1"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </span>
-        </label>
-        <span>{t("dark-mode")}</span>
-      </div>
+        </div>
+        {t("dark-mode")}
+      </label>
       <label htmlFor="primaryColor" className="flex flex-col gap-1">
         {t("primary-color")}
         <select
